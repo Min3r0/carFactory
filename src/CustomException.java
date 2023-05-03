8658801f2f5e9ce0859b64fbb4fb4f5c1329a68e
rorolastico <romainauge31@gmail.com>
2023-05-03 10:45:27 +0200

    add UserInputUtils.java change CustomException.java and add do while in Menu.java.

--- src/CustomException.java
+++ src/CustomException.java
@@ -3,17 +3,5 @@ public class CustomException extends Exception{
         super(message);
     }
 
-    public static void inputException(String userInput) {
 
-        try {
-            int userNumber = Integer.parseInt(userInput);
-            if (userNumber < 0 || userNumber > 5) {
-                throw new CustomException("L'entrée doit être comprise entre 0 et 5 !");
-            }
-        } catch (NumberFormatException e) {
-            System.err.println("L'entrée doit être un nombre !");
-        } catch (CustomException e) {
-            System.err.println(e.getMessage());
-        }
-    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Menu {
 //car's characteristics
     public static void mainMenu(List<Vehicule> vehiculeList){
-        System.out.println("""
+        String action ="";
+        do {
+            System.out.println("""
                 What do you want to do?
                 1) see the characteristics of a car.
                 2) see the characteristics of motor of a car.
@@ -12,34 +14,41 @@ public class Menu {
                 4) pass the technical control of a vehicle.
                 5) drive a vehicle.""");
 
-        Scanner scanner = new Scanner(System.in);
-        String action = scanner.nextLine();
-        switch (action){
-            case "1":
-                System.out.println("Which car?");
-                String characteristicsCar = scanner.nextLine();
-                Print.printCharacteristicsVehicule(vehiculeList.get(Integer.parseInt(characteristicsCar)-1));
-                break;
 
-            case "2":
-                System.out.println("Which car?");
-                String characteristicsMotor = scanner.nextLine();
-                Print.printCharacteristicsMotor(vehiculeList.get(Integer.parseInt(characteristicsMotor)-1).getMotor());
-                break;
+            Scanner scanner = new Scanner(System.in);
+             action = scanner.nextLine();
 
-            case "3":
-                System.out.println("Which car?");
-                String characteristicsMark = scanner.nextLine();
-                Print.printCharacteristicsMark(vehiculeList.get(Integer.parseInt(characteristicsMark)-1).getMark());
-                break;
+            UserInputUtils.inputException(action);
 
-            case "4":
-                System.out.println("Which car?");
-                String checkReady = scanner.nextLine();
-                Factory.checkReady(vehiculeList.get(Integer.parseInt(checkReady)-1));
-                break;
+            switch (action){
+                case "1":
+                    System.out.println("Which car?");
+                    String characteristicsCar = scanner.nextLine();
+                    Print.printCharacteristicsVehicule(vehiculeList.get(Integer.parseInt(characteristicsCar)-1));
+                    break;
 
-            case "5":
-        }
+                case "2":
+                    System.out.println("Which car?");
+                    String characteristicsMotor = scanner.nextLine();
+                    Print.printCharacteristicsMotor(vehiculeList.get(Integer.parseInt(characteristicsMotor)-1).getMotor());
+                    break;
+
+                case "3":
+                    System.out.println("Which car?");
+                    String characteristicsMark = scanner.nextLine();
+                    Print.printCharacteristicsMark(vehiculeList.get(Integer.parseInt(characteristicsMark)-1).getMark());
+                    break;
+
+                case "4":
+                    System.out.println("Which car?");
+                    String checkReady = scanner.nextLine();
+                    Factory.checkReady(vehiculeList.get(Integer.parseInt(checkReady)-1));
+                    break;
+
+                case "5":
+                    break;
+            }
+        }while(!action.equals("5"));
+
     }
 }
